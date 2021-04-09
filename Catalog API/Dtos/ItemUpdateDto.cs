b4f861ffe14c7ccb973ac2cc28ceba33b34b3991
HusseinYoussef <husseinymousa@gmail.com>
2021-04-10 00:22:01 +0200

    Add ItemUpdateDto

---
+++ Catalog API/Dtos/ItemUpdateDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalog_API.Dtos
+{
+    public class ItemUpdateDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 1000)]
+        public decimal Price { get; set; } 
+    }
+}
