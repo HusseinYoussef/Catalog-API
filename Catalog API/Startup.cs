637b8ae29a488fec448dc608b48b4049459feb52
HusseinYoussef <husseinymousa@gmail.com>
2021-04-11 23:33:55 +0200

    Add health checks

--- Catalog API/Startup.cs
+++ Catalog API/Startup.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Mime;
+using System.Text.Json;
 using Catalog_API.Repositories;
 using Catalog_API.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using MongoDB.Driver;
 
 namespace Catalog_API
 {
@@ -31,6 +32,14 @@ namespace Catalog_API
             services.Configure<MongoDbSettings>(_config.GetSection(nameof(MongoDbSettings)));
             services.AddScoped<IItemRepository, MongoItemRepository>();
             services.AddControllers();
+            var settings = _config.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            services.AddHealthChecks()
+                    .AddMongoDb(
+                        settings.ConnectionString,
+                        name: "mongodb",
+                        timeout: TimeSpan.FromSeconds(3),
+                        tags: new[] { "ready" }
+                    );
             services.AddSwaggerGen(c => 
             {
                 c.SwaggerDoc(name: "v1", new OpenApiInfo()
@@ -65,6 +74,31 @@ namespace Catalog_API
                     name: "default",
                     pattern: "{controller}/{action}/{id?}"
                 );
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions()
+                {
+                    Predicate = check => check.Tags.Contains("ready"),
+                    ResponseWriter = async (context, report) => {
+                        var result = JsonSerializer.Serialize(new
+                        {
+                            status = report.Status.ToString(),
+                            checks = report.Entries.Select(entry => new
+                            {
+                                name = entry.Key,
+                                status = entry.Value.Status.ToString(),
+                                exception = entry.Value.Exception?.Message ?? "none",
+                                duration = entry.Value.Duration.ToString()
+                            })
+                        });
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+                // Check if API is up and running
+                // Note: if Options isn't provided it will do all checks including Mongodb
+                endpoints.MapHealthChecks("/health/alive", new HealthCheckOptions()
+                {
+                    Predicate = (_) => false
+                });
             });
         }
     }
