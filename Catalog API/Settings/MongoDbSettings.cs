adf8fad94732d76f5ba3fa1d8601287f465326f2
HusseinYoussef <husseinymousa@gmail.com>
2021-04-11 11:41:27 +0200

    Add MongoDb credentials

--- Catalog API/Settings/MongoDbSettings.cs
+++ Catalog API/Settings/MongoDbSettings.cs
@@ -6,13 +6,15 @@ namespace Catalog_API.Settings
     { 
         public string DatabaseName { get; set; }
         public string CollectionName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public string Host { get; set; }
         public int Port { get; set; }
         public string ConnectionString
         {
             get
             {
-                return $"mongodb://{Host}:{Port}";
+                return $"mongodb://{Username}:{Password}@{Host}:{Port}";
             }
         }
     }
