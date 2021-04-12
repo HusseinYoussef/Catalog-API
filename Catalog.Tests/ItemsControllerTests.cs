5c92c66853fd2ab24e5b45ee0f751b453f6b91b4
HusseinYoussef <husseinymousa@gmail.com>
2021-04-12 18:08:15 +0200

    Add Initial Unit Testing Project

---
+++ Catalog.Tests/ItemsControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Catalog.Tests
+{
+    public class ItemsControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
